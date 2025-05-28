f2c9e4bf966bd818b4b3028a9d90a63461e93fb3
Rifqy Attaufi <rifqy1406@gmail.com>
2025-05-29 03:48:28 +0700

    integrasi unit budidaya

---
+++ lib/service/schedule_unit_notification.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:smart_farming_app/service/auth_service.dart';
+import 'package:http/http.dart' as http;
+
+class ScheduleUnitNotification {
+  final AuthService _authService = AuthService();
+
+  final String baseUrl = '${dotenv.env['BASE_URL']}/scheduledUnitNotification';
+
+  Future<Map<String, dynamic>> getScheduleUnitNotificationByUnitBudidaya(
+      String id) async {
+    final resolvedToken = await _authService.getToken();
+    final headers = {'Authorization': 'Bearer $resolvedToken'};
+    final url = Uri.parse('$baseUrl/unitBudidaya/$id');
+
+    try {
+      final response = await http.get(url, headers: headers);
+      final body = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        return {
+          'status': true,
+          'message': 'success',
+          'data': body['data'],
+        };
+      } else if (response.statusCode == 404) {
+        return {
+          'status': true,
+          'message': 'Data not found',
+          'data': [],
+        };
+      } else if (response.statusCode == 401) {
+        await _authService.refreshToken();
+        return await getScheduleUnitNotificationByUnitBudidaya(id);
+      } else {
+        return {
+          'status': false,
+          'message': body,
+        };
+      }
+    } catch (e) {
+      return {
+        'status': false,
+        'message': 'An error occurred: ${e.toString()}',
+      };
+    }
+  }
+}
