87788e428b333f6e408cddbe2a64bd8e2dc8711c
Rifqy Attaufi <rifqy1406@gmail.com>
2025-06-15 00:23:59 +0700

    remove unused import

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:smart_farming_app/service/auth_service.dart';
 import 'package:smart_farming_app/theme.dart';
